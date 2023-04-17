21dfcbd3f2a688b233e9b92a39bf684eb5ee8084
Stefan Todorovic <stefantodorovic@gmail.com>
2023-04-16 21:32:25 -0400

    Fix: album page next js image issue

--- src/pages/gallery/[gallery].js
+++ src/pages/gallery/[gallery].js
@@ -1,6 +1,5 @@
 import siteConfig from '@/websiteconfig';
 import { useState, useEffect } from 'react'
-import Image from 'next/image'
 
 
 const galleryData = siteConfig.gallery;
@@ -59,7 +58,7 @@ function GalleryViewer({ gallery }) {
               <h1 className="text-3xl font-bold mb-4">{`Gallery - ${gallery.name}`}</h1>
 
         <div className="relative h-96">
-          <Image src={gallery.images[currentImage]} alt="" layout="fill" objectFit="contain" />
+          <img src={gallery.images[currentImage]} alt="" layout="fill" objectFit="contain" />
           <button className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md" onClick={prevImage}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
