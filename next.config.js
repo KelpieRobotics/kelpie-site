12c0e6188e2f6cd6fe16146df0d7a6a5a36aecda
Stefan Todorovic <stefantodorovic@gmail.com>
2023-06-12 17:56:35 -0400

    updated images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
 		domains: ["refresh.kelpie-site.pages.dev", "kelpie-site.pages.dev", "kelpierobotics.com"],
+    unoptimized: true,
 	},
 }
 
