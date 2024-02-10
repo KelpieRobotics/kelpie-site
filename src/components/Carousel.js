8605c33ddc0bb7b7dcb1f231b5dc58c90a9abebd
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2024-02-10 15:00:52 -0500

    Carousel (BACK)

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,24 @@
 export const Carousel = ({cards})=>{
+    
     return (
         <div className="scroller">
             <div className="carousel">
-                {cards.map((card,index)=>{
-                    return(
-                        <div className="item" key={index}>
-                            {card}
-                        </div>
-                    )
-                })} 
+                    {cards.map((card,index)=>{
+                        return(
+                            <div className="item">
+                                {card}
+                            </div>
+                        )
+                    })} 
+            </div>
+            <div className="carousel">
+                    {cards.map((card,index)=>{
+                        return(
+                            <div className="item">
+                                {card}
+                            </div>
+                        )
+                    })} 
             </div>
         </div>   
     )
