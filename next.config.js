a4fa1025ebb9ac10ede1e9d2335d78c869f79086
Brooke-8 <bmacq014@uottawa.ca>
2026-05-11 21:15:43 -0400

    fix: format, packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["refresh.kelpie-site.pages.dev", "kelpie-site.pages.dev", "kelpierobotics.com"],
+    domains: [
+      "refresh.kelpie-site.pages.dev",
+      "kelpie-site.pages.dev",
+      "kelpierobotics.com",
+    ],
     unoptimized: true,
   },
 };
